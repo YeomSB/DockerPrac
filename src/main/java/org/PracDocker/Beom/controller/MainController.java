f04434abba9e5f4f8ec6c8271426aca84c8eb330
YeomSB <stanley0927@naver.com>
2024-03-30 18:38:07 +0900

    first commit

---
+++ src/main/java/org/PracDocker/Beom/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.PracDocker.Beom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello world";
+    }
+
+
+}

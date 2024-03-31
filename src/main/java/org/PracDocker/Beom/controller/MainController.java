002d42120f88b690b73f6dfcb4e27b4a7e50da31
YeomSB <stanley0927@naver.com>
2024-03-31 20:45:58 +0900

    기본구조

--- src/main/java/org/PracDocker/Beom/controller/MainController.java
+++ src/main/java/org/PracDocker/Beom/controller/MainController.java
@@ -1,10 +1,14 @@
 package org.PracDocker.Beom.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.PracDocker.Beom.config.S3Uploader;
 import org.PracDocker.Beom.dto.MainRequestDTO;
 import org.PracDocker.Beom.entity.MainEntity;
 import org.PracDocker.Beom.service.MainService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     private final MainService mainService;
+    private final S3Uploader s3Uploader;
+
 
     @GetMapping("/")
     public String home() {
@@ -29,6 +35,21 @@ public class MainController {
         return mainService.getName(id);
     }
 
+//    @PostMapping("/uploadimg")
+//    public BaseResponse<> create(
+//            @RequestPart(value = "postBoardReq") MultipartFile Req,
+//            @RequestPart(value = "boardImg", required = false) MultipartFile multipartFile){
+//
+//        String fileName = "";
+//        if(multipartFile != null){ // 파일 업로드한 경우에만
+//
+//            try{// 파일 업로드
+//                fileName = s3Uploader.upload(multipartFile, "images"); // S3 버킷의 images 디렉토리 안에 저장됨
+//                System.out.println("fileName = " + fileName);
+//            }catch (IOException e){
+//                return new BaseResponse<>(FAIL_FILE_CHANGE);
+//            }
+//        }
 
 
 }
